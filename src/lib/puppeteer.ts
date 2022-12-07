68d47edc0ab72d2dcc499bfc1807eb596beb5468
Glenn Francis <xglennfrancisx@gmail.com>
2022-12-07 21:29:26 +1000

    cleanup

--- src/lib/puppeteer.ts
+++ src/lib/puppeteer.ts
@@ -13,55 +13,17 @@ const exePath =
     ? '/usr/bin/google-chrome'
     : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
 
-const minimal_args = [
-  '--autoplay-policy=user-gesture-required',
-  '--disable-background-networking',
-  '--disable-background-timer-throttling',
-  '--disable-backgrounding-occluded-windows',
-  '--disable-breakpad',
-  '--disable-client-side-phishing-detection',
-  '--disable-component-update',
-  '--disable-default-apps',
-  '--disable-dev-shm-usage',
-  '--disable-domain-reliability',
-  '--disable-extensions',
-  '--disable-features=AudioServiceOutOfProcess',
-  '--disable-hang-monitor',
-  '--disable-ipc-flooding-protection',
-  '--disable-notifications',
-  '--disable-offer-store-unmasked-wallet-cards',
-  '--disable-popup-blocking',
-  '--disable-print-preview',
-  '--disable-prompt-on-repost',
-  '--disable-renderer-backgrounding',
-  '--disable-setuid-sandbox',
-  '--disable-speech-api',
-  '--disable-sync',
-  '--hide-scrollbars',
-  '--ignore-gpu-blacklist',
-  '--metrics-recording-only',
-  '--mute-audio',
-  '--no-default-browser-check',
-  '--no-first-run',
-  '--no-pings',
-  '--no-sandbox',
-  '--no-zygote',
-  '--password-store=basic',
-  '--use-gl=swiftshader',
-  '--use-mock-keychain'
-];
-
 async function getOptions(isDev: boolean) {
   let options: PuppeteerNodeLaunchOptions;
   if (isDev) {
     options = {
-      args: minimal_args,
+      args: [],
       executablePath: exePath,
       headless: true
     };
   } else {
     options = {
-      args: [...minimal_args, ...chrome.args],
+      args: chrome.args,
       executablePath: await chrome.executablePath,
       headless: chrome.headless
     };
